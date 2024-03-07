f512ee727a5ef2bd6eb8a394b1a2f00de407d6f8
sooraj <soorajkknmd@gmail.com>
2024-03-07 15:59:53 +0530

    q2

---
+++ answer2.py
@@ -0,0 +1,26 @@
+import json
+
+with open('data.json', 'r') as file:
+    data = json.load(file)
+    items = data['items']
+    new_list = []
+    for item in items:
+        arr = {
+                "name" : item.get('name'),
+                "categories" :item.get('categories'),
+                "image" : item.get('image'),
+                "base_price" : item.get('base_price'),
+                "availability_status":item.get('availability_status')
+                }
+        new_list.append(arr)
+        # print(f"{new_list}")     
+
+
+    user_input = input('Enter keyword: ')
+     
+    for sort in arr:
+        name = arr.get('name')
+        if user_input  == 'apple':
+            print(True)
+    else:
+        print(False)
